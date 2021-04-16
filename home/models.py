acc43ede37c9a6204eb60e1f337d8283419d0fb2
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-16 01:30:04 -0400

    removed unnecessary model

--- home/models.py
+++ home/models.py
@@ -4,5 +4,4 @@ from django.db import models
 class Home(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
